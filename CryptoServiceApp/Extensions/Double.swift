d906bbe2abc4a2964b7470d4482129178ab816ac
Pavel_Andreev <unspected777@gmail.com>
2023-04-09 20:28:06 -0700

    add stats functionally

--- CryptoServiceApp/Extensions/Double.swift
+++ CryptoServiceApp/Extensions/Double.swift
@@ -69,11 +69,12 @@ extension Double {
     /// Convert to 1.2345 to "1.23"
     /// ```
     func asNumberString() -> String {
-        let formatter = NumberFormatter()
-        formatter.minimumFractionDigits = 0 // Set minimum number of decimals
-        formatter.maximumFractionDigits = 2 // Set maximum number of decimals
-        let formattedNumber = formatter.string(from: NSNumber(value: self))
-        return formattedNumber!
+        return String(format: "%.2f", self)
+//        let formatter = NumberFormatter()
+//        formatter.minimumFractionDigits = 0 // Set minimum number of decimals
+//        formatter.maximumFractionDigits = 2 // Set maximum number of decimals
+//        let formattedNumber = formatter.string(from: NSNumber(value: self))
+//        return formattedNumber!
     }
     
     /// Converts double into string with percentage symbol
@@ -85,34 +86,42 @@ extension Double {
         
     }
     
-    
-    
-//    func formattedWithAbbreviations() -> String {
-//            let num = abs(Double(self))
-//            let sign = (self < 0) ? "-" : ""
-//
-//            switch num {
-//            case 1_000_000_000_000...:
-//                let formatted = num / 1_000_000_000_000
-//                let stringFormatted = formatted.asNumberString()
-//                return "\(sign)\(stringFormatted)Tr"
-//            case 1_000_000_000...:
-//                let formatted = num / 1_000_000_000
-//                let stringFormatted = formatted.asNumberString()
-//                return "\(sign)\(stringFormatted)Bn"
-//            case 1_000_000...:
-//                let formatted = num / 1_000_000
-//                let stringFormatted = formatted.asNumberString()
-//                return "\(sign)\(stringFormatted)M"
-//            case 1_000...:
-//                let formatted = num / 1_000
-//                let stringFormatted = formatted.asNumberString()
-//                return "\(sign)\(stringFormatted)K"
-//            case 0...:
-//                return self.asNumberString()
-//
-//            default:
-//                return "\(sign)\(self)"
-//            }
-//        }
+    /// Convert a Double to a String with K, M, Bn, Tr abbreviations.
+    /// ```
+    /// Convert 12 to 12.00
+    /// Convert 1234 to 1.23K
+    /// Convert 123456 to 123.45K
+    /// Convert 12345678 to 12.34M
+    /// Convert 1234567890 to 1.23Bn
+    /// Convert 123456789012 to 123.45Bn
+    /// Convert 12345678901234 to 12.34Tr
+    /// ```
+    func formattedWithAbbreviations() -> String {
+            let num = abs(Double(self))
+            let sign = (self < 0) ? "-" : ""
+
+            switch num {
+            case 1_000_000_000_000...:
+                let formatted = num / 1_000_000_000_000
+                let stringFormatted = formatted.asNumberString()
+                return "\(sign)\(stringFormatted)Tr"
+            case 1_000_000_000...:
+                let formatted = num / 1_000_000_000
+                let stringFormatted = formatted.asNumberString()
+                return "\(sign)\(stringFormatted)Bn"
+            case 1_000_000...:
+                let formatted = num / 1_000_000
+                let stringFormatted = formatted.asNumberString()
+                return "\(sign)\(stringFormatted)M"
+            case 1_000...:
+                let formatted = num / 1_000
+                let stringFormatted = formatted.asNumberString()
+                return "\(sign)\(stringFormatted)K"
+            case 0...:
+                return self.asNumberString()
+
+            default:
+                return "\(sign)\(self)"
+            }
+        }
 }
