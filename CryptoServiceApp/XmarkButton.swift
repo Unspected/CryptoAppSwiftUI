21631d7e554fd95ab695f5b0743a24506d001ca9
Pavel_Andreev <unspected777@gmail.com>
2023-04-10 16:46:24 -0700

    add portfolio list

---
+++ CryptoServiceApp/XmarkButton.swift
@@ -0,0 +1,30 @@
+//
+//  XmarkButton.swift
+//  CryptoServiceApp
+//
+//  Created by Pavel Andreev on 4/9/23.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        
+        Button {
+            dismiss()
+            
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        }
+
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
